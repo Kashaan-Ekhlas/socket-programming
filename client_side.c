5289680d2691058bcf706f2941e9f4a047c81b29
Kashaan-Ekhlas <kashaanekhlas@gmail.com>
2026-05-09 19:46:46 +0530

    Established Socket Connection to a google server because yes

--- client_side.c
+++ client_side.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 int main(){
-  int fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (fd == -1){
-    perror("socket oopsies");	
+  int socketFD = socket(AF_INET, SOCK_STREAM, 0);
+  if (socketFD == -1){
+    perror("Socket Creation Oopsies");	
+  }
+
+  char* ip = "142.250.188.46";
+  int port = 80;
+
+  struct sockaddr_in address = {
+    .sin_family = AF_INET,
+    .sin_port = htons(port),
+  };
+  inet_pton(AF_INET, ip, &address.sin_addr.s_addr);
+
+  int result = connect(socketFD, &address, sizeof(address));
+  if (result == 0){
+    printf("Socket Connection Success!\n");
+  }
+  else{ 
+    perror("Socket Connection Oopsies\n");
   }
   return 0;
 }
