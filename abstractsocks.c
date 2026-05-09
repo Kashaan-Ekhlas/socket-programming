2fc7cedb73995e8ef985540238033a7e9214ad91
Kashaan-Ekhlas <kashaanekhlas@gmail.com>
2026-05-10 03:25:04 +0530

    Updated socket library to include listening to any ip addresses, and setup basic server side code

--- abstractsocks.c
+++ abstractsocks.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <string.h>
 
 int createTCPIPv4Socket(){
   int socketFD = socket(AF_INET, SOCK_STREAM, 0);
@@ -18,9 +19,15 @@ struct sockaddr_in createIPv4Address(const char* ip, const int port){
     .sin_family = AF_INET, 
     .sin_port = htons(port) 
   };
-  if (inet_pton(AF_INET, ip, &address.sin_addr) <= 0){
-    perror("Inet PTON Oopsies");
-    exit(1);
+  if (ip == NULL || strlen(ip) == 0){
+    address.sin_addr.s_addr = INADDR_ANY;
   }
+  else{
+    if (inet_pton(AF_INET, ip, &address.sin_addr) <= 0){
+      perror("Inet PTON Oopsies");
+      exit(1);
+    }
+  }
+  
   return address;
 }
--- server_side.c
+++ server_side.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <>
+#include "abstractsocks.h"
 
 int main(){
-  
+  int serverSocketFD = createTCPIPv4Socket();
+  // empty string basically means listen to requests from any ip
+  struct sockaddr_in serverAddress = createIPv4Address("", 2000);
   return 0;
 }
