aa836a5a53b2d9a87d0caac6d442d478749d487a
Kashaan-Ekhlas <kashaanekhlas@gmail.com>
2026-05-09 20:07:26 +0530

    Sending google cute stuff

--- client_side.c
+++ client_side.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 int main(){
   int socketFD = socket(AF_INET, SOCK_STREAM, 0);
   if (socketFD == -1){
-    perror("Socket Creation Oopsies");	
+    perror("Socket Creation Oopsies");
+    return 1;
   }
 
   char* ip = "142.250.188.46";
@@ -15,14 +18,21 @@ int main(){
     .sin_family = AF_INET,
     .sin_port = htons(port),
   };
-  inet_pton(AF_INET, ip, &address.sin_addr.s_addr);
+  inet_pton(AF_INET, ip, &address.sin_addr);
 
-  int result = connect(socketFD, &address, sizeof(address));
+  int result = connect(socketFD, (struct sockaddr*) &address, sizeof(address));
   if (result == 0){
     printf("Socket Connection Success!\n");
   }
   else{ 
-    perror("Socket Connection Oopsies\n");
+    perror("Socket Connection Oopsies");
   }
+  
+  char* buffer = "meow_maww";
+  ssize_t sent = send(socketFD, buffer, strlen(buffer), 0);
+  if (sent == -1){
+    perror("Transmission Failed");
+  }
+  close(socketFD);
   return 0;
 }
